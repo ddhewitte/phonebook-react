8936ed98d3a6a80418764e6cc324d44449d35e97
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-11-15 18:54:57 +0700

    outer template

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      Phonebook App
+    <div className="container">
+      <div className="bg-white min-h-screen">
+        <div className="max-w-4xl mx-auto px-4 py-8">
+          <div className="">
+            header
+          </div>
+
+          <div>The list</div>
+        </div>
+      </div>
     </div>
   )
 }
