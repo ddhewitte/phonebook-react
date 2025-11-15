a0dcdd1ecbf2c370d0290fed241bef13d8a3e8b6
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-11-15 19:25:58 +0700

    simple design using tailwind and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,43 @@ function App() {
     <div className="container">
       <div className="bg-white min-h-screen">
         <div className="max-w-4xl mx-auto px-4 py-8">
-          <div className="">
-            header
+
+          <h1 className="text-2xl font-bold text-black mb-2">Phonebook App</h1>
+
+          <div className="max-w-full p-4  bg-[#6a5269] rounded-md mb-2 gap-4 text-white">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <div>
+                  <label htmlFor="name">Name</label>
+                  <input type="text" id="name" className="w-full bg-white rounded-sm text-black p-2" placeholder="Enter name.."></input>
+                </div>
+                <div>
+                  <label htmlFor="phone">Phone Number</label>
+                  <input type="tel" id="phone" className="w-full bg-white rounded-sm text-black p-2" placeholder="Enter phone number.."></input>
+                </div>
+              </div>
+
+              <div className="mt-2">
+                <button type="button" className="w-auto h-auto p-2 bg-black text-white rounded-md hover:bg-gray-800">Add to phonebook</button>
+              </div>
           </div>
 
-          <div>The list</div>
+          <div className="max-w-full p-4 bg-white border border-gray-500 rounded-md mb-2 gap-4 text-white">
+            <h2 className="font-bold text-black">Phonebook Lists</h2>
+
+            <div className="flex justify-between items-center text-black p-2">
+                <div>1</div>
+                <div>Joko Sampurno</div>
+                <div>082399787666</div>
+                <div>add/edit</div>
+            </div>
+
+            <div className="flex justify-between items-center text-black p-2">
+                <div>1</div>
+                <div>Joko Sampurno</div>
+                <div>082399787666</div>
+                <div>add/edit</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
