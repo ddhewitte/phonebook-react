ed4630571665fe82851315b071c9dbaa239c977a
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-11-16 14:45:08 +0700

    Add data phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,31 @@ const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
 function App() {
 
   const [person, setPerson] = useState([]);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
 
   useEffect(() => {
     fetchPhoneBook();
   }, [])
 
+  //display data
   async function fetchPhoneBook(){
     const response = await axios.get(API_ENDPOINT);
     setPerson(response.data);
   }
+
+  //add data
+  const addPerson = async (e) => {
+    e.preventDefault();
+    if(!name || !phone) return;
+    const responseAdd = await axios.post(API_ENDPOINT, { name, phone});
+    if(responseAdd){
+      setName('');
+      setPhone('');
+      fetchPhoneBook();
+    }
+  }
+
   return (
     <div className="container">
       <div className="bg-white min-h-screen">
@@ -24,20 +40,39 @@ function App() {
           <h1 className="text-2xl font-bold text-black mb-2">Phonebook App</h1>
 
           <div className="max-w-full p-4  bg-[#6a5269] rounded-md mb-2 gap-4 text-white">
+            <form onSubmit={addPerson}>
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <div>
-                  <label htmlFor="name">Name</label>
-                  <input type="text" id="name" className="w-full bg-white rounded-sm text-black p-2" placeholder="Enter name.."></input>
-                </div>
-                <div>
-                  <label htmlFor="phone">Phone Number</label>
-                  <input type="tel" id="phone" className="w-full bg-white rounded-sm text-black p-2" placeholder="Enter phone number.."></input>
-                </div>
+                  <div>
+                    <label htmlFor="name">Name</label>
+                    <input 
+                      type="text" 
+                      id="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)} 
+                      className="w-full bg-white rounded-sm text-black p-2" 
+                      placeholder="Enter name.." >
+                    </input>
+                  </div>
+                  <div>
+                    <label htmlFor="phone">Phone Number</label>
+                    <input 
+                      type="tel" 
+                      id="phone"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      className="w-full bg-white rounded-sm text-black p-2" 
+                      placeholder="Enter phone number.." ></input>
+                  </div>
               </div>
 
               <div className="mt-2">
-                <button type="button" className="w-auto h-auto p-2 bg-black text-white rounded-md hover:bg-gray-800">Add to phonebook</button>
+                <button 
+                  type="submit" 
+                  className="w-auto h-auto p-2 bg-black text-white rounded-md hover:bg-gray-800">
+                  Add to phonebook
+                </button>
               </div>
+              </form>
           </div>
 
           <div className="max-w-full p-4 bg-white border border-gray-500 rounded-md mb-2 gap-4 text-white">
